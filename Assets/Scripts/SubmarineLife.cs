0694900e5d1975746d4143f886d440ea75f73608
Antoine <a.leroux@rubika-edu.com>
2021-02-10 16:55:54 +0100

    submarine life + feedback color when submarine is close to fregate

---
+++ Assets/Scripts/SubmarineLife.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubmarineLife : MonoBehaviour
+{
+    [Header("Submarine Life")]
+    public float maxLife;
+    public float damageCoef;
+    public float currentLife;
+
+    [Header("Feedback")]
+    public float maxDistance;
+    public Color colorFar;
+    public Color colorClose;
+    [Space]
+    public GameObject colorFeedback;
+    public GameObject fregate;
+
+    [Header("UI")]
+    public Image lifeBar;
+
+    private float distanceFromFregate;
+    private SubmarineTriggerZone submarineTriggerScript;
+
+    private void Start()
+    {
+        colorFeedback.SetActive(false);
+
+        submarineTriggerScript = GetComponentInChildren<SubmarineTriggerZone>();
+
+        currentLife = maxLife;
+    }
+
+    private void Update()
+    {
+        if (submarineTriggerScript.fregateIsAbove)
+        {
+            DamageOverTime();
+
+            lifeBar.fillAmount = currentLife / maxLife;
+
+            if (currentLife > maxLife)
+            {
+                currentLife = maxLife;
+            }
+            if (currentLife < 0)
+            {
+                currentLife = 0;
+            }
+        }
+
+        distanceFromFregate = Vector3.Distance(transform.position, fregate.transform.position);
+
+        if (distanceFromFregate <= maxDistance)
+        {
+            ChangeColorOverDistance();
+        }
+    }
+
+    private void DamageOverTime()
+    {
+        currentLife -= damageCoef * Time.deltaTime;
+    }
+
+    private void ChangeColorOverDistance()
+    {
+        colorFeedback.SetActive(true);
+
+        colorFeedback.transform.position = new Vector3(fregate.transform.position.x + 2, fregate.transform.position.y + 2, fregate.transform.position.z + 2);
+
+        colorFeedback.GetComponent<SpriteRenderer>().color = Color.Lerp(colorClose, colorFar, distanceFromFregate / maxDistance);
+    }
+}

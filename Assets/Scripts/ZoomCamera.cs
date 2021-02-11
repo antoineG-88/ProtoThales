a90f309e480428985a21a099b5740f4971728d48
Antoine <a.leroux@rubika-edu.com>
2021-02-11 23:43:00 +0100

    Merge zoom + submarine work

--- Assets/Scripts/ZoomCamera.cs
+++ Assets/Scripts/ZoomCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ZoomCamera : MonoBehaviour
 {
     private Vector3 startTouch;
+    public BatimentController batimentControllerScript;
 
     [Header("Zoom Settings")]
     public float zoomMin;
@@ -14,44 +15,46 @@ public class ZoomCamera : MonoBehaviour
     public float MouseZoomSpeed;
     public float touchZoomSpeed;
 
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(batimentControllerScript.batimentSelected == null)
         {
-            //startTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Input.GetMouseButtonDown(0))
+            {
+                //startTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            startTouch = GetWorldPoisition(0);
+                startTouch = GetWorldPoisition(0);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                //Vector3 direction = startTouch - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                Vector3 direction = startTouch - GetWorldPoisition(0);
+                Camera.main.transform.position += direction;
+
+                //Limit Camera movement 
+                transform.position = new Vector3(Mathf.Clamp(transform.position.x, -13, 13), Mathf.Clamp(transform.position.y, 33, 47), Mathf.Clamp(transform.position.z, -46, 14));
+            }
+            else if (Input.touchCount == 2)
+            {
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                Vector2 touchZeroPreviousPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePreviousPos = touchOne.position - touchOne.deltaPosition;
+
+                float previousMagnitude = (touchZeroPreviousPos - touchOnePreviousPos).magnitude;
+                float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+                float difference = currentMagnitude - previousMagnitude;
+
+                zoom(difference * touchZoomSpeed);
+            }
+
+            zoom(Input.GetAxis("Mouse ScrollWheel") * MouseZoomSpeed);
         }
-
-        if (Input.GetMouseButton(0))
-        {
-            //Vector3 direction = startTouch - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Vector3 direction = startTouch - GetWorldPoisition(0);
-            Camera.main.transform.position += direction;
-
-            //Limit Camera movement 
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -13, 13), Mathf.Clamp(transform.position.y, 33, 47), Mathf.Clamp(transform.position.z, -46, 14));
-        }
-        else if (Input.touchCount == 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            Vector2 touchZeroPreviousPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePreviousPos = touchOne.position - touchOne.deltaPosition;
-
-            float previousMagnitude = (touchZeroPreviousPos - touchOnePreviousPos).magnitude;
-            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
-
-            float difference = currentMagnitude - previousMagnitude;
-
-            zoom(difference * touchZoomSpeed);
-        }
-
-        zoom(Input.GetAxis("Mouse ScrollWheel") * MouseZoomSpeed);
-
-        
     }
 
     //ortographic cam

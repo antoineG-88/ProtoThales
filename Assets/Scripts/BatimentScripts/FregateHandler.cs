e34f6534e5258ab424bc863198cfe01ef3c9ede0
Antoine <a.leroux@rubika-edu.com>
2021-02-16 15:31:56 +0100

    Remove units to use equipment + remove speed handler

--- Assets/Scripts/BatimentScripts/FregateHandler.cs
+++ Assets/Scripts/BatimentScripts/FregateHandler.cs
@@ -4,71 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 public class FregateHandler : MonoBehaviour
 {
-    public int maxUnitAvailable;
     public float deepSonarChargeTime;
     public float[] deepSonarDistanceSteps;
     public Sprite[] deepSonarDistanceStepImages;
     public GameObject sonarEffectPrefab;
     public Image deepSonarCharge;
-    public Text unitsControlNumberText;
-    public Text unitsDeepSonarNumberText;
-    public Text unitsHullSonarNumberText;
-    public Text unitsAvailableNumberText;
     public Image hullSonarImage;
     public Color hullSonarActivatedColor;
     public Transform submarine;
     public PinHandler pinHandler;
 
     [HideInInspector] public Zone currentZone;
-    [HideInInspector] public bool isUsingHullSonar;
-    private int unitsAvailable;
-    private int unitEngagedOnControl;
-    private int unitEngagedOnHullSonar;
-    private int unitEngagedOnDeepSonar;
     private float currentSonarCharge;
     private BatimentController batimentController;
     private Color hullSonarBaseColor;
     private Fregate fregate;
 
-    [Header("HullSonar")]
-    public float maxDetectionDistanceSubmarine;
-    public Color colorFar;
-    public Color colorClose;
-    [Space]
-    public GameObject colorFeedback;
-
-    private float submarineDistance;
+    private bool deepSonar;
+    private bool isUsinghullSonar;
     
 
     void Start()
     {
         fregate = GetComponent<Fregate>();
-        unitEngagedOnDeepSonar = 0;
-        unitEngagedOnControl = 0;
-        unitEngagedOnHullSonar = 0;
-        unitsAvailable = maxUnitAvailable;
         hullSonarBaseColor = hullSonarImage.color;
-        colorFeedback.SetActive(false);
     }
 
     void Update()
     {
         currentZone = ZoneHandler.GetCurrentZone(fregate.currentPosition);
 
-        fregate.unitsOnControl = unitEngagedOnControl;
-
-        isUsingHullSonar = unitEngagedOnHullSonar >= 1;
-
-        unitsHullSonarNumberText.text = unitEngagedOnHullSonar.ToString();
-        unitsControlNumberText.text = unitEngagedOnControl.ToString();
-        unitsDeepSonarNumberText.text = unitEngagedOnDeepSonar.ToString();
-        unitsAvailableNumberText.text = unitsAvailable.ToString();
-
-        if (unitEngagedOnDeepSonar == 0)
+        if (!deepSonar)
         {
             deepSonarCharge.fillAmount = 0;
         }
-        else if (unitEngagedOnDeepSonar == 1)
+        else if (deepSonar)
         {
             currentSonarCharge += Time.deltaTime;
             if (currentSonarCharge > deepSonarChargeTime)
@@ -77,27 +47,8 @@ public class FregateHandler : MonoBehaviour
             }
             deepSonarCharge.fillAmount = currentSonarCharge / deepSonarChargeTime;
         }
-
-        submarineDistance = Vector2.Distance(fregate.currentPosition, SeaCoord.Planify(submarine.position));
-
-        if (submarineDistance <= maxDetectionDistanceSubmarine && unitEngagedOnHullSonar == 1)
-        {
-            ChangeColorOverDistance();
-        }
-        else
-        {
-            colorFeedback.SetActive(false);
-        }
     }
 
-    private void ChangeColorOverDistance()
-    {
-        colorFeedback.SetActive(true);
-
-        colorFeedback.transform.position = new Vector3(fregate.transform.position.x + 2, fregate.transform.position.y + 2, fregate.transform.position.z + 2);
-
-        colorFeedback.GetComponent<SpriteRenderer>().color = Color.Lerp(colorClose, colorFar, submarineDistance / maxDetectionDistanceSubmarine);
-    }
 
     private void UseSonar()
     {
@@ -136,43 +87,28 @@ public class FregateHandler : MonoBehaviour
 
         pinHandler.CreateDeepSonarPin(distanceStep, direction, fregate.currentDirection, fregate.currentPosition);
         Instantiate(sonarEffectPrefab, fregate.transform.position + Vector3.up * 0.1f, Quaternion.identity);
-        unitsAvailable++;
-        unitEngagedOnDeepSonar--;
         currentSonarCharge = 0;
+
+        deepSonar = false;
     }
 
-
-    public void AddControl(int value)
+    public void ActivateDeepSonar()
     {
-        if (unitsAvailable - value >= 0 && unitsAvailable - value <= maxUnitAvailable && unitEngagedOnControl + value >= 0)
-        {
-            unitsAvailable -= value;
-            unitEngagedOnControl += value;
-        }
+        deepSonar = true;
     }
 
     public void ActivateHullSonar()
     {
-        if (unitsAvailable > 0 && unitEngagedOnHullSonar == 0)
+        if (!isUsinghullSonar)
         {
-            unitsAvailable--;
-            unitEngagedOnHullSonar++;
+            isUsinghullSonar = true;
             hullSonarImage.color = hullSonarActivatedColor;
-        }
-        else if (unitEngagedOnHullSonar == 1)
-        {
-            unitsAvailable++;
-            unitEngagedOnHullSonar--;
-            hullSonarImage.color = hullSonarBaseColor;
-        }
-    }
 
-    public void AddDeepSonar(int value)
-    {
-        if (unitsAvailable > 0 && unitEngagedOnDeepSonar == 0)
+        }
+        else if (isUsinghullSonar)
         {
-            unitsAvailable -= value;
-            unitEngagedOnDeepSonar += value;
+            isUsinghullSonar = false;
+            hullSonarImage.color = hullSonarBaseColor;
         }
     }
 }
